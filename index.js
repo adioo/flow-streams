a22238220a7c805abf0d34bfd63dc1d87ca611fb
Adrian Ottiker <adrian@ottiker.com>
2017-02-22 16:12:37 +0100

    prepare to update to new data stream

--- index.js
+++ index.js
@@ -52,21 +52,25 @@ exports.json = {
         return next ? next(null, data) : data;
     },
 
-    stringify: (scope, inst, args, data, next) => {
+    stringify: (scope, inst, args, data, stream, next) => {
 
-        for (let key in args) {
+        /*if (Object.keys(args || {}) > 0) {
+            for (let key in args) {
 
-            if (!data[key]) {
-                return next(new Error('Flow-streams.stringify: No streams to found.'));
+                if (!data[key]) {
+                    return next(new Error('Flow-streams.stringify: No streams to found.'));
+                }
+
+                if (data[key] instanceof Array) {
+                    data[key].forEach((stream, index) => {
+                        data[key][index] = stream.pipe(JSONStream.stringify(args[key][0], args[key][1], args[key][2]));
+                    });
+                } else {
+                    data[key] = data[key].pipe(JSONStream.stringify(args[key][0], args[key][1], args[key][2]));
+                }
             }
-            if (data[key] instanceof Array) {
-                data[key].forEach((stream, index) => {
-                    data[key][index] = stream.pipe(JSONStream.stringify(args[key][0], args[key][1], args[key][2]));
-                });
-            } else {
-                data[key] = data[key].pipe(JSONStream.stringify(args[key][0], args[key][1], args[key][2]));
-            }
-        }
+        } else {*/
+        stream = stream.pipe(JSONStream.stringify(args[0], args[1], args[2]));
 
         next(null, data);
     }
